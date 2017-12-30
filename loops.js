800202a95c5049ffe41e53bd48a7ab196078a2c0
asmaa.abbas@wework.com <asmaa.abbas@wework.com>
2017-12-30 20:12:30 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,14 @@
+function forLoop (array) {
+  array.push("I am 1 strange loop.");
+  for (var i = 1; i < 25; i++){
+    array.push(`I am ${i} strange loops.`);
+  }
+  return array;
+}
+
+function whileLoop (n) {
+  while(n > 0){
+    --n;
+  }
+  console.log('done');
+}
